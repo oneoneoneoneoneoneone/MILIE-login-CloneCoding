429dadbd4620cb2dcb6018dd477e7b13c9a05417
oneoneoneoneoneoneone <0ne1_1@naver.com>
2023-03-29 15:01:27 +0900

    [Feat] Apple 로그인을 위한 암호화 클래스 추가

---
+++ millieLoginCloneCoding/Scene/Cryptography.swift
@@ -0,0 +1,58 @@
+//
+//  Cryptography.swift
+//  millieLoginCloneCoding
+//
+//  Created by hana on 2023/03/29.
+//
+
+import CryptoKit
+import FirebaseAuth
+
+class Cryptography{
+    ///firebase Apple 로그인 - nonce 암호화
+    //로그인 요청마다 임의의 문자열인 'nonce'가 생성되며, 이 nonce는 앱의 인증 요청에 대한 응답으로 ID 토큰이 명시적으로 부여되었는지 확인하는 데 사용됩니다. 재전송 공격을 방지
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: [Character] = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+              var random: UInt8 = 0
+              let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+              if errorCode != errSecSuccess {
+                fatalError(
+                  "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+                )
+              }
+              return random
+            }
+
+        randoms.forEach { random in
+            if remainingLength == 0 {
+                return
+            }
+
+            if random < charset.count {
+                result.append(charset[Int(random)])
+                remainingLength -= 1
+            }
+        }
+    }
+        return result
+    }
+    
+    ///로그인 요청시 SHA256 해시 암호화
+    @available(iOS 13, *)
+    func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
+}
