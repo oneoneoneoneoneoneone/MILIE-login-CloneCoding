b04b5e84eec5ff80cdac7e9b12fbc33005a28d6d
oneoneoneoneoneoneone <0ne1_1@naver.com>
2023-04-03 23:15:35 +0900

    [Feat] 회원가입 화면추가, 스토리보드 분리

---
+++ millieLoginCloneCoding/Scene/Join/JoinProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  JoinProfileViewController.swift
+//  millieLoginCloneCoding
+//
+//  Created by hana on 2023/04/03.
+//
+
+import UIKit
+
+class JoinProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ millieLoginCloneCoding/Scene/Join/JoinViewController.swift
@@ -0,0 +1,16 @@
+//
+//  JoinViewController.swift
+//  millieLoginCloneCoding
+//
+//  Created by hana on 2023/04/03.
+//
+
+import UIKit
+
+class JoinViewController: UIViewController {
+    @IBOutlet weak var phoneInputStackView: InputStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
