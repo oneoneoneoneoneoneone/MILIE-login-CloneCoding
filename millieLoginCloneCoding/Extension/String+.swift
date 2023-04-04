24a8c3326577788fa907403d052675f0090995ad
oneoneoneoneoneoneone <0ne1_1@naver.com>
2023-04-05 00:29:21 +0900

    [Refactor] String 익스텐션 분리

---
+++ millieLoginCloneCoding/Extension/String+.swift
@@ -0,0 +1,24 @@
+//
+//  String+.swift
+//  millieLoginCloneCoding
+//
+//  Created by hana on 2023/04/04.
+//
+
+import Foundation
+
+extension String {
+    func base64Encoded() -> String? { // base64 인코딩 수행 실시
+        if let data = self.data(using: .utf8) {
+            return data.base64EncodedString()
+        }
+        return ""
+    }
+
+    func base64Decoded() -> String? { // base64 디코딩 수행 실시
+        if let data = Data(base64Encoded: self, options: .ignoreUnknownCharacters) {
+            return String(data: data, encoding: .utf8)
+        }
+        return ""
+    }
+}
