4a0dbc58a5fe459b258a4199348e4a16cadcd05d
oneoneoneoneoneoneone <0ne1_1@naver.com>
2023-04-09 17:46:28 +0900

    [Feat] getplistValue Type 추가

--- millieLoginCloneCoding/Extension/Bundle+plist.swift
+++ millieLoginCloneCoding/Extension/Bundle+plist.swift
@@ -9,22 +9,26 @@ import Foundation
 
 extension Bundle {
     
-    func getValue(key: String) -> String {
-        // forResource에다 plist 파일 이름을 입력해주세요.
-        guard let filePath = Bundle.main.path(forResource: "APIKey", ofType: "plist"),
+    func getplistValue(path: String, key: String) -> String {
+        guard let filePath = Bundle.main.path(forResource: path, ofType: "plist"),
               let plistDict = NSDictionary(contentsOfFile: filePath) else {
-            fatalError("Couldn't find file 'SecureAPIKeys.plist'.")
+            fatalError("Couldn't find file '\(path).plist'.")
         }
-        
-        // plist 안쪽에 사용할 Key값을 입력해주세요.
         guard let value = plistDict.object(forKey: key) as? String else {
-            fatalError("Couldn't find key 'API_Key' in 'SecureAPIKeys.plist'.")
+            fatalError("Couldn't find key \(key) in '\(path).plist'.")
         }
         
-        // 또는 키 값을 통해 직접적으로 불러줄 수도 있어요.
-        // guard let value = plistDict["API_KEY"] as? String else {
-        //     fatalError("Couldn't find key 'API_Key' in 'SecureAPIKeys.plist'.")
-        // }
+        return value
+    }
+    
+    func getplistValue(path: String, key: String) -> [[String: Any]] {
+        guard let filePath = Bundle.main.path(forResource: path, ofType: "plist"),
+              let plistDict = NSDictionary(contentsOfFile: filePath) else {
+            fatalError("Couldn't find file '\(path).plist'.")
+        }
+        guard let value = plistDict.object(forKey: key) as? [[String: Any]] else {
+            fatalError("Couldn't find key \(key) in '\(path).plist'.")
+        }
         
         return value
     }
