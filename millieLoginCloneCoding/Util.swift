412726273a270a03c033236b7d8217b3489bcef6
oneoneoneoneoneoneone <0ne1_1@naver.com>
2023-04-18 12:35:16 +0900

    [Feat] 휴대폰번호 정규식 추가

---
+++ millieLoginCloneCoding/Util.swift
@@ -0,0 +1,13 @@
+//
+//  Util.swift
+//  millieLoginCloneCoding
+//
+//  Created by hana on 2023/04/17.
+//
+
+import Foundation
+
+struct Util{
+    static let phoneRegex = /^010-?([0-9]{4})-?([0-9]{4})$/
+    static let phone10Regex = /^01([1|6|7|8|9])-?([0-9]{3})-?([0-9]{4})$/
+}
