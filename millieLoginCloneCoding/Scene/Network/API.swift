5c770a41dae4457149cba4f76a496ed8910899e1
oneoneoneoneoneoneone <0ne1_1@naver.com>
2023-04-25 03:26:56 +0900

    [Refactor] API 클래스에서 URLSession 호출 메소드까지 포함, NetworkManager 클래스는 외부 요청을 기능별로 분리한 메소드만 가짐

--- millieLoginCloneCoding/Scene/Network/API.swift
+++ millieLoginCloneCoding/Scene/Network/API.swift
@@ -9,37 +9,53 @@ import Foundation
 import FirebaseAuth
 
 struct API{
-    static let scheme = "https"
     static let host = "millie-login-default-rtdb.firebaseio.com"
     
-    
-    func getIdToken(completionHandler: @escaping ((String) -> Void)){
-        let currentUser = Auth.auth().currentUser
-        currentUser?.getIDTokenForcingRefresh(true) { idToken, error in
-            if let error = error {
-                print(error.localizedDescription)
-                return;
-            }
-            guard let idToken = idToken else {
-                return
-            }
-            
-            completionHandler(idToken)
-        }
-    }
-    
-    func getURLComponents(_ queryname: String! = nil, _ queryvalue: String! = nil) -> URLComponents{
-        var components = URLComponents()
-        components.scheme = API.scheme
-        components.host = API.host
-        components.path = "/users.json"
-        
-        components.queryItems = [
-            URLQueryItem(name: "print", value: "pretty"),
-            URLQueryItem(name: "orderBy", value: "\"\(queryname!)\""),
-            URLQueryItem(name: "equalTo", value: "\"\(queryvalue!)\"")
+    func getURLComponents(_ queryname: String? = nil, _ queryvalue: String? = nil) -> URLComponents?{
+        var components = URLComponents(string: "https://millie-login-default-rtdb.firebaseio.com")
+        components?.host = API.host
+        components?.path = "/users.json"
+
+        components?.queryItems = [
+            URLQueryItem(name: "print", value: "pretty")
         ]
+        
+        guard let queryname = queryname,
+              let queryvalue = queryvalue else {return components}
+        
+        components?.queryItems!.append(URLQueryItem(name: "orderBy", value: "\"\(queryname)\""))
+        components?.queryItems!.append(URLQueryItem(name: "equalTo", value: "\"\(queryvalue)\""))
         
         return components
     }
+    
+    
+    func getUserData(url: URL) async throws -> [String : User]?{
+        let urlRequest = URLRequest(url: url)
+
+        //dataTask에 있는 error 리턴 값이 없음.
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        guard let statuseCode = (response as? HTTPURLResponse)?.statusCode,
+              (200...299).contains(statuseCode) else {throw NSError(domain: "response", code: 0)}
+
+        let user = try JSONDecoder().decode([String:User].self, from: data)
+
+        return user
+    }
+
+    func postUserData(url: URL, user: User) async throws -> String?{
+        var urlRequest = try URLRequest(url: url, method: .post)
+        urlRequest.httpBody = try JSONEncoder().encode(user)
+
+        //dataTask에 있는 error 리턴 값이 없음.
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        guard let statuseCode = (response as? HTTPURLResponse)?.statusCode,
+              (200...299).contains(statuseCode) else {throw NSError(domain: "response", code: 0)}
+
+        let name = try JSONDecoder().decode([String:String].self, from: data).first?.value
+
+        return name
+    }
 }
+
+
